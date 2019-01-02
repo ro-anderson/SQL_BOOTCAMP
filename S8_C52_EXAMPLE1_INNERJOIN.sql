eb1dbe1a10e0b977371f9c7cb63b08726214b95f
didierr <didier.rda@gmail.com>
2019-01-01 22:12:38 -0200

    Versão abreviando o nome da tabela.

--- S8_C52_EXAMPLE1_INNERJOIN.sql
+++ S8_C52_EXAMPLE1_INNERJOIN.sql
@@ -1,3 +1,3 @@
-﻿SELECT film.title, language.name AS movie_language
+﻿SELECT film.title, lan.name AS movie_language
 FROM film
-INNER JOIN language ON language.language_id = film.language_id;
+INNER JOIN language AS lan ON lan.language_id = film.language_id;
