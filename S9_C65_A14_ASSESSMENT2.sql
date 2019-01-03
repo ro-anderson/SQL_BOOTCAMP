82f82978b8510be0210084ae18e433990d3a3554
didierr <didier.rda@gmail.com>
2019-01-03 02:35:52 -0200

    S9_C65_A14_ASSESSMENT2 criado.

---
+++ S9_C65_A14_ASSESSMENT2.sql
@@ -0,0 +1,4 @@
+﻿SELECT A.name, B.starttime
+FROM cd.facilities AS A
+INNER JOIN cd.bookings AS B ON A.facid = B.facid
+WHERE name LIKE '%Tennis Court%' AND extract(month FROM starttime) = 9 AND extract(day FROM starttime)=21;
