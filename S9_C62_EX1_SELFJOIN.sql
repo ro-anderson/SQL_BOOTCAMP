6ec521f8bd9811b076bccf3ffbf9c89fb243dc00
didierr <didier.rda@gmail.com>
2019-01-02 22:33:02 -0200

    S9_C62_EX1_SELFJOIN criado

---
+++ S9_C62_EX1_SELFJOIN.sql
@@ -0,0 +1,4 @@
+﻿SELECT A.customer_id,A.first_name, A.last_name,B.customer_id,B.first_name, B.last_name
+FROM customer AS A,/*JOIN customer AS B ON A.first_name = B.last_name */
+customer AS B
+WHERE A.first_name = B.last_name;
