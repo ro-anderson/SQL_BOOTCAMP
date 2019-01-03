fd6b60e72279a7394ea63e7e8e2b4645bc41c390
didierr <didier.rda@gmail.com>
2019-01-03 21:37:54 -0200

    S11_EX1_CREATE_TABLE criado

---
+++ S11_C71_EX1_CREATE_TABLE.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE account(
+user_id serial PRIMARY KEY,
+user_name VARCHAR(50) UNIQUE NOT NULL,
+password VARCHAR(50) NOT NULL,
+email VARCHAR(355) UNIQUE NOT NULL,
+created_on TIMESTAMP NOT NULL,
+last_login TIMESTAMP);
