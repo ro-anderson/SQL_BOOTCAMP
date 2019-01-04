aa3c951a673f68a58aa1e84d94ee23afc43b21f8
didierr <didier.rda@gmail.com>
2019-01-03 22:13:21 -0200

    S11_EX3_CREATE_TABLE criado

---
+++ S11_C71_EX3_CREATE_TABLE.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE account_role(
+user_id integer NOT NULL,
+role_id integer NOT NULL,
+grant_date timestamp WITHOUT TIME ZONE,
+PRIMARY KEY (user_id,role_id),
+
+CONSTRAINT account_role_role_id_fkey FOREIGN KEY (role_id)
+	REFERENCES role (role_id) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE NO ACTION,
+CONSTRAINT account_role_user_id_fkey FOREIGN KEY (user_id)
+	REFERENCES account (user_id) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE NO ACTION 
+);
