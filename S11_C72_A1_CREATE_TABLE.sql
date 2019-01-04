e487caa0dd52ba4075b824a641adc57b0ef5017d
didierr <didier.rda@gmail.com>
2019-01-03 22:27:56 -0200

    S11_C72_A1_CREATE_TABLE criado

---
+++ S11_C72_A1_CREATE_TABLE.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE customer(
+customer_id integer PRIMARY KEY,
+first_name VARCHAR(50) NOT NULL,
+last_name VARCHAR(50) NOT NULL,
+email VARCHAR(300) UNIQUE NOT NULL,
+signup_date DATE NOT NULL,
+num_min_spent_site INTERVAL NOT NULL
+);
