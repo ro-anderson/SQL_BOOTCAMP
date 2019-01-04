2249f7eeae4ba0f364c8b3a8e064d6f84a767536
didierr <didier.rda@gmail.com>
2019-01-03 22:35:17 -0200

    S11_C72_A1_CREATE_TABLE customer_id ajustado de integer p serial.

--- S11_C72_A1_CREATE_TABLE.sql
+++ S11_C72_A1_CREATE_TABLE.sql
@@ -1,5 +1,5 @@
-﻿CREATE TABLE customer(
-customer_id integer PRIMARY KEY,
+﻿CREATE TABLE customer_data(
+customer_id serial PRIMARY KEY,
 first_name VARCHAR(50) NOT NULL,
 last_name VARCHAR(50) NOT NULL,
 email VARCHAR(300) UNIQUE NOT NULL,
