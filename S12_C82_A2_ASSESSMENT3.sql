04c6cc466cd6a29bfd332d89a50dc712818a2fb0
didierr <didier.rda@gmail.com>
2019-01-06 17:13:17 -0200

    Adicionado NOT NULL para a variavel Phone.

--- S12_C82_A2_ASSESSMENT3.sql
+++ S12_C82_A2_ASSESSMENT3.sql
@@ -4,7 +4,7 @@
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	homeroom_number integer NOT NULL,
-	phone VARCHAR(100) UNIQUE,
+	phone VARCHAR(100) NOT NULL UNIQUE,
 	email VARCHAR(100) UNIQUE,
         graduation_year integer NOT NULL
 );
