4ed6af43a76c17708c2fef665b4935e593e65890
didierr <didier.rda@gmail.com>
2019-01-06 17:09:30 -0200

    S12_C82_A2_ASSESSMENT3.sql criado.

---
+++ S12_C82_A2_ASSESSMENT3.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE students
+(
+	students_id serial PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	homeroom_number integer NOT NULL,
+	phone VARCHAR(100) UNIQUE,
+	email VARCHAR(100) UNIQUE,
+        graduation_year integer NOT NULL
+);
+
